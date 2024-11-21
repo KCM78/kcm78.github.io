00bb7a84aec0899a0c8092a74ccab3ed3fab0c49
KCM78 <kenny.mooney@gmail.com>
2024-11-21 11:23:57 +0000

    website overhaul

--- components/Page/PageWrapper.js
+++
@@ -1,8 +0,0 @@
-export default function PageWrapper({ children, title }) {
-  return (
-    <div className='w-3/4 container mx-auto mt-12'>
-      <h1 className='leading-tight text-3xl text-black font-medium'>{title}</h1>
-      {children}
-    </div>
-  )
-}
